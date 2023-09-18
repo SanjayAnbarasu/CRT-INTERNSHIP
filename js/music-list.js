270cfed6fb2247edacdfa38460fa13a074222d68
SanjayAnbarasu <145284364+SanjayAnbarasu@users.noreply.github.com>
2023-09-18 12:46:40 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,51 @@
+let allMusic = [
+  {
+    name: "NAN GALLI-GOOD NIGHT",
+    artist: "SEAN ROLDAN",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "CHINANCHIRU NILAVE PS-2",
+    artist: "AR RAHMAN",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "JAWAN PREVIEW-JAWAN",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "HAYODA-JAWAN",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "HUKUM-JAILER",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "JAILER THEME-JAILER",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "JAILER FLUTE THEME-JAILER",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "BLOODY SWEET-LEO",
+    artist: "ANIRUDH RAVICHANDHAR",
+    img: "music-10",
+    src: "music-10"
+  },
+  ];
+  
